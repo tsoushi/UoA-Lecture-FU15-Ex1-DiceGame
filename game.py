fff1b16d9490c8ee9fa38817730f15ddeac5ac9c
Matsuda Soushi <79128170+tsoushi@users.noreply.github.com>
2023-06-09 11:11:50 +0900

    Won/lost message added

--- game.py
+++ game.py
@@ -8,3 +8,8 @@ dice2 = random.randint(1, 6)
 print('Die 1:', dice1)
 print('Die 2:', dice2)
 print('Total value: ', dice1 + dice2)
+
+if dice1 + dice2 > 7:
+    print('You won!')
+else:
+    print('You lost...')
