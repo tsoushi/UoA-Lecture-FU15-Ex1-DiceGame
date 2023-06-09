cc35ffd779b9f14aca0a9339498db582a8d7b02f
Matsuda Soushi <79128170+tsoushi@users.noreply.github.com>
2023-06-09 11:04:26 +0900

    Dice game: initial revision

---
+++ game.py
@@ -0,0 +1,10 @@
+import random
+
+print('Rolling dice...')
+
+dice1 = random.randint(1, 6)
+dice2 = random.randint(1, 6)
+
+print('Die 1:', dice1)
+print('Die 2:', dice2)
+print('Total value: ', dice1 + dice2)
